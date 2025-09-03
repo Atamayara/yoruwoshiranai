a55adf5a9505bd53cb7d6377b87f7735d35c9c1d
Atamayara <otsukike@icloud.com>
2025-09-03 16:14:56 +0900

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/yoruwoshiranai/',
+    base: 'https://atamayara.github.io/yoruwoshiranai/',
     build: {
         target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'],
         outDir: './dist/',
